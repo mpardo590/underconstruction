a23b98e32051f6eabbf5db9b5b875b4ac37b004a
mpardo590 <mpardo590@gmail.com>
2019-07-16 19:45:14 -0400

    build

---
+++ build/precache-manifest.4c48f070aae42e8e4afcb7c3cef6560b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dfdb3ad193c14b4248a3380ccd4284",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9afe1d47f8c3e28886c",
+    "url": "/static/css/2.86465799.chunk.css"
+  },
+  {
+    "revision": "ca0fa8bb4fd2e9e3a839",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a9afe1d47f8c3e28886c",
+    "url": "/static/js/2.b48860c6.chunk.js"
+  },
+  {
+    "revision": "ca0fa8bb4fd2e9e3a839",
+    "url": "/static/js/main.b436364f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
